78a7f46246f1b3cb03ce358cf7427f95c6722abb
yuzhangbit <yu.zhang.bit@gmail.com>
2018-10-09 17:25:29 +0800

    add test main loop for gtest

---
+++ common/common_tests.cpp
@@ -0,0 +1,14 @@
+/* Copyright (c) 2018, Yu Zhang, Intelligent Vehicle Research Center(IVRC), 
+ * Beijing Institute of Technology.
+ * All rights reserved.
+ *        Files: math_tests.cpp
+ *   Created on: 10, 08, 2018 
+ *       Author: Yu Zhang
+ *        Email: yu.zhang.bit@gmail.com
+ */
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
