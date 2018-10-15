2a61d60ed21a12ba4019054aa0bd9cfb3a96d2fc
yuzhangbit <yu.zhang.bit@gmail.com>
2018-10-15 14:18:38 +0800

    match the image name

--- scripts/docker_build_image.sh
+++ scripts/docker_build_image.sh
@@ -3,4 +3,4 @@ set -e
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${CURRENT_DIR}
-docker build -t xenial .
+docker build -it bit_xenial .
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -3,4 +3,4 @@ set -e
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${CURRENT_DIR} && cd ..
-docker run -d -t --name xenial -v $(pwd):/home/port_apollo ubuntu
+docker run -d -it --name xenial -v $(pwd):/home/port_apollo bit_xenial
